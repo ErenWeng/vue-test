5f71858196e9b63e0bd1e3a70d0a6cbdbe3efca0
Eren <f245986@gmail.com>
2024-03-06 21:58:10 +0800

    chore: add tailwind

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // WSL2 not update: https://cn.vitejs.dev/guide/troubleshooting.html#vite-does-not-detect-a-file-change
+  server: {
+    watch: {
+      usePolling: true
+    }
+  }
 })
